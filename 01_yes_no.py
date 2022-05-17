a110d3f429baa6a046ecac3cdbbf848454387d70
RomeThompson <thomps.rome@awatapu.school.nz>
2022-05-18 10:20:15 +1200

    Developing a function

---
+++ 01_yes_no.py
@@ -0,0 +1,15 @@
+ show_instructions = " "
+ while show_instructions.lower() != "xxx":
+    # Ask the user if they have played before
+    show_instructions = input("Have you played this game before?")
+
+    # If they say yes, output 'program continues'
+    if show_instructions == "yes" or show_instructions == "y":
+        print("program continues")
+
+    elif show_instructions == "no" or show_instructions == "n":
+        print("Program Continues")
+    # If they say no, output 'display instructions'
+
+    else:
+        print("Error please enter yes or no")
